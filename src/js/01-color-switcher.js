9d23113fa5b0d502c7e7986782cb733ac7ab25fc
tasita <uatasita@gmail.com>
2023-05-14 23:33:39 +0300

    Initial commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    stopBtn.removeAttribute('disabled');
+    
+    timerId = setInterval(changeBodyColor, 1000)
+});
+
+stopBtn.addEventListener('click', () => {
+    startBtn.removeAttribute('disabled');
+    stopBtn.disabled = true;
+    
+    clearInterval(timerId);    
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBodyColor() {
+   document.body.style.backgroundColor = getRandomHexColor(); 
+};
